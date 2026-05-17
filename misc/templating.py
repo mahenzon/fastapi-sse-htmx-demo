ae2bfa8424c7b224a9cdcd0547f3782fa304c32b
Suren Khorenyan <surenkhorenyan@gmail.com>
2026-05-17 17:00:17 +0300

    create render_string util

--- misc/templating.py
+++ misc/templating.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from fastapi.templating import Jinja2Templates
 
 from config import BASE_DIR
@@ -6,3 +8,10 @@ TEMPLATES_DIR = BASE_DIR / "templates"
 templates = Jinja2Templates(
     directory=TEMPLATES_DIR,
 )
+
+
+def render_string(
+    name: str,
+    **context: Any,  # noqa: ANN401
+) -> str:
+    return templates.env.get_template(name).render(**context)
