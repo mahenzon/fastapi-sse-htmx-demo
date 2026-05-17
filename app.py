ae65a9bed5f4a68c9c9fea7aa1c618d6ab60d3ec
Suren Khorenyan <surenkhorenyan@gmail.com>
2026-05-17 14:21:59 +0300

    add style css

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
+from config import BASE_DIR
 from misc.templating import templates
 from orders.views import router
 
+STATIC_DIR = BASE_DIR / "static"
 app = FastAPI(
     title="Orders API: SSE + HTMX Demo",
+)
+app.mount(
+    "/static",
+    StaticFiles(directory=STATIC_DIR),
+    name="static",
 )
 app.include_router(router)
 
